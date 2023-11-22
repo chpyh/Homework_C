fa0b0693c73b8c3233ebb9b1ece7f1d9f0c7382e
chp_h <veron.grigoreva@gmail.com>
2023-11-22 20:58:39 +0300

    Задача 2

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿//Задача 1
-Console.Clear();
+/*Console.Clear();
 Console.Write("Введите  число: ");
 int n = int.Parse(Console.ReadLine()!);
 if(n % 7 ==0 && n % 23 == 0)
@@ -9,4 +9,38 @@ Console.WriteLine($"{n} является числом, одновременно 
 else
 {
     Console.WriteLine($"{n} не является числом, одновременно кратным и 7, и 23");
+}
+*/
+// Задача 2
+Console.Clear();
+Console.Write("Введите координату X: ");
+int X = int.Parse(Console.ReadLine()!);
+while(X == 0)
+{
+    Console.Write("Введите координату X: ");
+    X = int.Parse(Console.ReadLine()!);
+}
+Console.Write("Введите координату Y: ");
+int Y = int.Parse(Console.ReadLine()!);
+while(Y == 0)
+{
+    Console.Write("Введите координату Y: ");
+    Y = int.Parse(Console.ReadLine()!);
+}
+
+if(X > 0 && Y > 0)
+{
+    Console.WriteLine("I");
+}
+else if(X < 0 && Y > 0)
+{
+  Console.WriteLine("II");  
+}
+else if(X < 0 && Y <0)
+{
+  Console.WriteLine("III");  
+}
+else
+{
+    Console.WriteLine("IV");  
 }
