98f36a791ae538f305662cff27c7f1403abd2027
chp_h <veron.grigoreva@gmail.com>
2023-11-22 20:57:01 +0300

    Задача 1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿//Задача 1
+Console.Clear();
+Console.Write("Введите  число: ");
+int n = int.Parse(Console.ReadLine()!);
+if(n % 7 ==0 && n % 23 == 0)
+{
+Console.WriteLine($"{n} является числом, одновременно кратным и 7, и 23 ");
+}
+else
+{
+    Console.WriteLine($"{n} не является числом, одновременно кратным и 7, и 23");
+}
